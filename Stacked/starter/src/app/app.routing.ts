ec9dbb02a68f5da8b2a4bbe3b2b09b39dde0ca19
Voblaco <fearess7@gmail.com>
2019-06-15 13:46:39 +0300

    Added bootstrap

---
+++ Stacked/starter/src/app/app.routing.ts
@@ -0,0 +1,39 @@
+import { Routes } from '@angular/router';
+
+import { AdminLayoutComponent } from './core';
+import { AuthLayoutComponent } from './core';
+
+export const AppRoutes: Routes = [
+  {
+    path: '',
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: './dashboard/dashboard.module#DashboardModule'
+      },
+      {
+        path: 'docs',
+        loadChildren: './docs/docs.module#DocsModule'
+      }
+    ]
+  },
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: 'account',
+        loadChildren: './account/account.module#AccountModule'
+      },
+      {
+        path: 'error',
+        loadChildren: './error/error.module#ErrorModule'
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'error/404'
+  }
+];
